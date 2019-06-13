287e2d933cc8fda5124ee004ed042d1c619bef70
JoneSaldanha <jonesaldanha@gmail.com>
2019-06-13 00:20:45 -0300

    Criando uma fábrica

--- CodeLab00/Exercicio02.dart
+++ CodeLab00/Exercicio02.dart
@@ -12,14 +12,19 @@ abstract class Shape {
 }
 
 class Circle implements Shape {
+
 	final num radius;
+
 	Circle(this.radius);
 	num get area => pi * pow(radius, 2);
+
 }
 
 class Square implements Shape {
+
 	final num side;
-	Square(this.side);
+	
+  Square(this.side);
 	num get area => pow(side, 2);
 }
 
@@ -30,26 +35,25 @@ class CircleMock implements Circle {
 }
 
 
-// Shape shapeFactory(String type){
-// 	if (type == 'circle') return Circle(2);
-// 	if (type == 'square') return Square(2);
-// 	throw 'Forma invalida! $type';
-// }
+Shape shapeFactory(String type){
+ 	if (type == 'circle') return Circle(2);
+ 	if (type == 'square') return Square(2);
+ 	throw 'Forma invalida! $type';
+ }
 
 
 
 void main(){
 
-	//Instanciando normalmente os objetos de 'circle' e 'square' 
-	//final circle = Circle(2);
-	//final square = Square(2);
+	final circle0 = Circle(2);
+	final square0 = Square(2);
 
-	//Utilizando uma função de alto nivel para o Instanciamento
-	//final circle = shapeFactory('circle');
-	//final square = shapeFactory('square');
+	final circle1 = shapeFactory('circle');
+	final square1 = shapeFactory('square');
 	
-	final circle = Shape('circle');
-	final square = Shape('square');
-	print(circle.area);
+	final circle1 = Shape('circle');
+	final square1 = Shape('square');
+	
+  print(circle.area);
 	print(square.area);
 }
