faaecea8cc1a1cfbf93fcc8d0eb6f6053e098e8e
JoneSaldanha <jonesaldanha@gmail.com>
2019-06-12 23:34:09 -0300

    Criando uma classe simples

---
+++ CodeLab00/Exercicio00.dart
@@ -0,0 +1,38 @@
+class Bicicleta{
+  
+  int cadencia;
+  int _velocidade = 0;
+  int get velocidade => _velocidade;
+  int engrenagens;
+  
+  Bicicleta(this.cadencia, this.engrenagens);
+            
+  @override
+  String toString() => 'Cadencia: $cadencia'+ "\n"+
+                       'Velocidade: $_velocidade'+ "\n"+
+                       'Engrenagens: $engrenagens ';
+  
+  
+  
+  
+  void reduzVel(int decremento) {
+    
+    _velocidade -= decremento;
+
+  }
+
+void aumentaVel(int incremento) {
+  
+   _velocidade += incremento;
+
+  }
+  
+}
+
+void main() {
+  
+  var bike = Bicicleta(2, 1);
+  
+  print(bike);
+  
+}
